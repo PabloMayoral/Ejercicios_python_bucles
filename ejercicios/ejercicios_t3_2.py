d0702c955d39fa78f1e3e94c38eafe71db220fab
a814014 <pablo.martin.external@atos.net>
2022-02-09 10:43:06 +0100

    añadido comentarios y fixeo de alguna linea de codigo

--- ejercicios/ejercicios_t3_2.py
+++ ejercicios/ejercicios_t3_2.py
@@ -9,8 +9,8 @@ x = 0
 #creo un bucle while que se recoorra con la condición de que se repita hasta llegar al final de la extension de
 #de la arraylist, dentro del bucle while introduzco un if para especificar que si el resto es = 0 sabremos que 
 #el indice del elemento será par e incremento la i para continuar el bucle.
-while x <= longitud:
-    if x % 2 == 0:
+while x < longitud:
+    if x % 2 == 1:
         print(asignaturas[x]) 
     x += 1
 print('------------------------------------')
@@ -61,7 +61,10 @@ msg2 = 'mensaje 2'
 arrayList = [cadena_tupla,cadena_lista,cadena_booleana,msg1,msg2]
 
 
-
+#creo un bucle for que recorra la lista, dentro le pongo un if que tenga la condicion de si el elemento que esta 
+#recorriendo es de tipo tupla realice un bucle for que recorra esa tupla y muestre su contenido, esto lo haré 2 veces
+#ya que tambien me piden mostrar el elemento que sea una lista y recorrerlo por dentro, además añado una variable llamada 'w'
+#que funcionará como un contador.
 for i in arrayList:
     w = 1
     
@@ -101,6 +104,11 @@ print('------------------------------------')
 # divisible entre 2 o si lo es entre 5, 
 # en caso contrario indicará que no es divisible entre 2 ni 5.
 
+#creo un bucle for y le especifico el rango que debe repetirse, me debe mostrar del 5 al 51, si especifico un in range(5,51)
+#no me mostrará el 51 por lo que el rango deberá ser range(5,52)
+#dentro del bucle uso el metodo usado en el primer ejercicio que es el modulo (%) para saber si es divisible o no entre 2 o 5
+
+
 for i in range(5,52):
     if i % 2 == 0 or i % 5 == 0:
         print('El número '+ str(i) +' es divisible entre 2 o 5')
@@ -119,6 +127,12 @@ print('------------------------------------')
 #  Muestra por pantalla cuantas aes aparecen en el texto.
 #  Muestra por pantalla el numero total de letras que hay en el texto. 
  
+#creo la variable mensaje que contendrá el string con el que vamos a trabajar, para evitarnos añadir más condiciones en el if
+#usaré el metodo .lower() para que el texto esté todo en minusculas
+#creo 3 tipos de contadores, uno para las aes, otro para los numeros y otro para los espacios en blanco y signos de puntuación
+#creo un bucle for que me recorra el string y dentro especifico las condiciones con un 3 if, cada uno fuera del otro
+# por ultimo en los print indico que se muestre los contadores y en el ultimo print al querer contar las letras
+# cogeremos la extension del string y le restare la cantidad de nuemros y la cantidad de espacios en blanco y signos de puntuación. 
 mensaje = 'El episodio piloto se estrenó en más de 6 servicios en línea de video bajo demanda el 27 de mayo de 2015. La temporada 1 se estrenó en USA Network el 24 de junio de 2015 y la segunda temporada el 13 de julio de 2016. La tercera temporada, de 10 episodios, se estrenó el 11 de octubre de 2017. En diciembre de 2017 se renovó la cuarta y última temporada de la serie, que fue estrenada el 6 de octubre de 2019.'
 mensajeMinusculas = mensaje.lower()
 
