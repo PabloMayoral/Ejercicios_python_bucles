08347a7e3442886cdcfa69bf96e293703a674855
a814014 <pablo.martin.external@atos.net>
2022-02-11 11:33:02 +0100

    apuntes de los ejercicios de listas y tuplas

--- apuntes/listas_tuplas.py
+++ apuntes/listas_tuplas.py
@@ -7,13 +7,32 @@ print(lst2)
 lst3 = lst_names + lst2
 print(lst3)'''
 
-listaNumeros = [1,2,3,4]
-j = 0
+
+
+'''j = 0
 for x in listaNumeros:
     if x % 2 == 0:
         listaNumeros[j] = 'a'
     else:
         listaNumeros[j] = 'b'
     j += 1
+print(listaNumeros)'''
+
+listaNumeros = [1,2,3,5,6,7]     
+tuplaNumeros = (2,3,7,8,1,4,0)
+estan = []
+noEstan = []
+
+for x in tuplaNumeros:
+    if x in listaNumeros:
+         estan.append(x)
+    else:
+         noEstan.append(x)
+
+print('los numeros ', estan, 'Estan en la lista de numeros', noEstan,'no estan en la lista')
+j = 0
+for i in listaNumeros:
+    if i not in estan:
+        listaNumeros[j] = 12
+    j += 1
 print(listaNumeros)
-       
