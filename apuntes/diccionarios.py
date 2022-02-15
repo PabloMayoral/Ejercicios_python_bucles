4b421bca868faad8f91ac9aeeb5e9c489b3dc3e2
a814014 <pablo.martin.external@atos.net>
2022-02-15 12:42:47 +0100

    añadido los apuntes de los dicionarios

--- apuntes/diccionarios.py
+++ apuntes/diccionarios.py
@@ -1,1 +1,14 @@
 #Apuntesd e los ejercicios de los diccionarios
+diccionario ={'Fecha nacimiento': '14/04/1990', 'nombre':'Pedro','Apellidos':'Gonzalez','color de pelo':'castaño','color de ojos':'verdes','DIN':'51320584C','Peso':65}
+
+diccionario['Peso'] += 0.5  
+print(diccionario['Peso'])
+
+for k in diccionario.keys():
+    print(k)
+for v in diccionario.values():
+    print(v)
+
+for key,value in diccionario.items():
+    print(key+': '+str(value))
+
