f2e1a68a447412e4b5ed146c09131078ed5c71ed
a814014 <pablo.martin.external@atos.net>
2022-02-22 13:41:01 +0100

    añadido apuntes de funciones

---
+++ apuntes/funciones.py
@@ -0,0 +1,13 @@
+def funcion1 (a,b):
+    return a+ b
+print(funcion1(10,5))
+     
+
+def helloWorldFunc():
+    print('Hello World Function!!')
+helloWorldFunc()
+
+def helloWorldFunc2(frase = 'Hello World Function!!' ):
+    print(frase)
+helloWorldFunc2('Hola Pablo')
+helloWorldFunc2()
